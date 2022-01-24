bdc99bb3e34a507fe0f730cd181db315247cec1f
sumithrashanmugam <98223532+sumithrashanmugam@users.noreply.github.com>
2022-01-24 17:08:51 +0530

    Add files via upload

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -3,7 +3,7 @@ package assignment;
 public class PrimeNumbers {
 public static void main(String[]args) {
 	int num = 91;
-	boolean flag = true;
+	boolean flag = false;
 	for (int i = 2; i <= num / 2; ++i) {
 	if (num % i == 0) {
 	flag = true;
